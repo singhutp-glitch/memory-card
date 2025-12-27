1072b048b93b917bca86183f067f52746f503b8e
singhutp-glitch <singhutp@gmail.com>
2025-12-27 10:55:40 +0530

    Make giphy api work

--- src/components/cardSpace.jsx
+++ src/components/cardSpace.jsx
@@ -1,4 +1,31 @@
 export default function CardSpace()
 {
-    return(<></>)
+    return(<><div className="cardSec">
+        
+        <div className="card">
+            <img className='cardImg' src="#" height='400px' width='400px' />
+            {
+                // newImage(document.querySelector('img.cardImg'))
+            }
+        </div>
+        
+    </div></>)
+}
+
+function newImage(img)
+{
+    const target='';
+
+    let url='https://api.giphy.com/v1/gifs/translate?api_key=76sHFQVOfK3kGZLUONxubMZ4T6t3Mgdg&s='+target;
+    
+    fetch(url)
+      .then(function(response) {
+        return response.json();
+      })
+      .then(function(response) {
+        img.src = response.data.images.original.url;
+        console.log('new');
+      }).catch(function(error){
+        console.log('error: '+error);
+      });
 }
