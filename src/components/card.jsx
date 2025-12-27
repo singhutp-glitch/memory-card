2e6cd53b37b9db0e01bfb92728d7bbccab3d1358
singhutp-glitch <singhutp@gmail.com>
2025-12-27 12:05:06 +0530

    Make img persist after reload

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -2,28 +2,14 @@ import { useState,useEffect } from "react";
 
 export default function Card()
 {
-    const [imgUrl,setImgUrl]=useState(null);
+    const GIF_ID = "lXo8uSnIkaB9e"; // pikachu
 
-    useEffect(()=>{
-        async function newImage()
-        {
-            const target='ironman';
-
-            let url='https://api.giphy.com/v1/gifs/translate?api_key=76sHFQVOfK3kGZLUONxubMZ4T6t3Mgdg&s='+target;
-            
-            const res=await fetch(url);
-            const data=await res.json();
-            setImgUrl(data.data.images.fixed_width.url);
-        }
-        newImage();
-        
-    },[])
-
+    const still = `https://media.giphy.com/media/${GIF_ID}/giphy_s.gif`;
     return(<>
         
         <div className="card">
-            <img className='cardImg' src={imgUrl} height='300px' width='250px' />
-            <div className="name">pikachu</div>
+            <img className='cardImg' src={still} height='300px' width='250px' />
+            <div className="name">iron man</div>
         </div></>)
 }
 
