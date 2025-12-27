60defeaf1d650d42ad9bcbec8702e90d27fccb07
singhutp-glitch <singhutp@gmail.com>
2025-12-27 11:27:44 +0530

    Make Card element work

--- src/components/cardSpace.jsx
+++ src/components/cardSpace.jsx
@@ -1,31 +1,31 @@
+import { useState,useEffect } from "react";
+
 export default function CardSpace()
 {
+    const [imgUrl,setImgUrl]=useState(null);
+
+    useEffect(()=>{
+        async function newImage()
+        {
+            const target='ironman';
+
+            let url='https://api.giphy.com/v1/gifs/translate?api_key=76sHFQVOfK3kGZLUONxubMZ4T6t3Mgdg&s='+target;
+            
+            const res=await fetch(url);
+            const data=await res.json();
+            setImgUrl(data.data.images.fixed_width.url);
+        }
+        newImage();
+        
+    },[])
+
     return(<><div className="cardSec">
         
         <div className="card">
-            <img className='cardImg' src="#" height='400px' width='400px' />
-            {
-                // newImage(document.querySelector('img.cardImg'))
-            }
+            <img className='cardImg' src={imgUrl} height='300px' width='250px' />
+            <div className="name">pikachu</div>
         </div>
         
     </div></>)
 }
 
-function newImage(img)
-{
-    const target='';
-
-    let url='https://api.giphy.com/v1/gifs/translate?api_key=76sHFQVOfK3kGZLUONxubMZ4T6t3Mgdg&s='+target;
-    
-    fetch(url)
-      .then(function(response) {
-        return response.json();
-      })
-      .then(function(response) {
-        img.src = response.data.images.original.url;
-        console.log('new');
-      }).catch(function(error){
-        console.log('error: '+error);
-      });
-}
